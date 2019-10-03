fb337f8d49272ddda7565a28ebf0fa10d031c06d
Andrew Webb <androbwebb@gmail.com>
2019-10-03 21:28:44 +0100

    Add dynamic endpoints for webb

--- src/remotes.ts
+++ src/remotes.ts
@@ -3,9 +3,19 @@ import 'dotenv/config';
 const MAPBOX_API_ENDPOINT = 'https://api.mapbox.com';
 const ACCESS_TOKEN = process.env.MAPBOX_API_TOKEN;
 
+type GeocodeApiName = 'mapbox' | 'andrewwebb';
+type GeocodeApiEndpointFn = (remote: Remote) => string;
+type geocodeApis = { [key in GeocodeApiName]: GeocodeApiEndpointFn};
+
 type Remote = {
   name: string;
-  location: string;
+  location?: string;
+  api?: GeocodeApiName;
+};
+
+const geocodeEndpoints: geocodeApis = {
+  mapbox: (remote: Remote) => `${MAPBOX_API_ENDPOINT}/geocoding/v5/mapbox.places/${remote.location}.json?access_token=${ACCESS_TOKEN}&types=place&limit=1`,
+  andrewwebb: (remote: Remote) => `https://andrewwebb.herokuapp.com/api/where-are-you/?access_token=${process.env.ANDREWWEBB_API_TOKEN}`
 };
 
 export type RemoteWithLocationData = Remote & {
@@ -22,7 +32,7 @@ const remotes: Remote[] = [
   { name: 'Ivan', location: 'Sofia, Bulgaria' },
   { name: 'Luke', location: 'Raleigh, North Carolina, United States' },
   { name: 'Andy', location: 'Omaha, Nebraska, United States' },
-  { name: 'Andrew', location: 'London, England' },
+  { name: 'Webb', api: 'andrewwebb'},
   { name: 'SF HQ', location: 'San Francisco, California, United States' },
   { name: 'Kevin', location: 'Salt Lake City, Utah, United States' },
   { name: 'Greg and Mike', location: 'St. Louis, Missouri, United States' },
@@ -31,13 +41,16 @@ const remotes: Remote[] = [
 
 export function fetchRemotes(): Promise<RemoteWithLocationData[]> {
   const tasks = remotes.map(async remote => {
+    let endpointBuilder = geocodeEndpoints.mapbox;
+    if (remote.api) {
+      endpointBuilder = geocodeEndpoints[remote.api];
+    }
+
     const coordLngLat: [number, number] | null = await fetch(
-      `${MAPBOX_API_ENDPOINT}/geocoding/v5/mapbox.places/${
-        remote.location
-      }.json?access_token=${ACCESS_TOKEN}&types=place&limit=1`,
+      endpointBuilder(remote)
     )
       .then(res => res.json())
-      .then(json =>
+      .then((json) =>
         json.features && json.features.length > 0
           ? json.features[0].center
           : null,
